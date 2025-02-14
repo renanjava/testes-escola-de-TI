c56d3ad766b031f4bf51b61d06e2dd3de385a7ef
renanjava <renancvr777@gmail.com>
2025-02-14 20:03:08 -0300

    teste do workflow check

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,7 +6,6 @@ import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
-
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
