f19d4c34f3029e782f9deb3e011286a44ca2063a
renanjava <renancvr777@gmail.com>
2025-02-20 20:23:38 -0300

    test: primeiro teste validando o construtor do AuthLoginDto

---
+++ src/auth/dto/__tests__/auth-login.dto.spec.ts
@@ -0,0 +1,25 @@
+import { AuthLoginFactory, AuthLoginProps } from '../auth-login.dto'
+import { AuthLoginDataBuilder } from '../helper/auth-login-data-builder'
+
+let props: AuthLoginProps
+
+describe('AuthLoginDto unit tests', () => {
+  beforeEach(() => {
+    props = AuthLoginDataBuilder({} as AuthLoginProps)
+  })
+
+  it('constructor method', () => {
+    let sut = AuthLoginFactory.create('  usuario teste ', '83dskjwj222')
+
+    expect(sut.user).toBe('usuario teste')
+    expect(sut.password).toBe('83dskjwj222')
+
+    sut = AuthLoginFactory.create(
+      props.user as string,
+      props.password as string,
+    )
+
+    expect(sut.user).toBe(props.user)
+    expect(sut.password).toBe(props.password)
+  })
+})
--- src/auth/dto/auth-login.dto.ts
+++ src/auth/dto/auth-login.dto.ts
@@ -1,14 +1,27 @@
-import { IsString } from 'class-validator'
+import { IsString, MaxLength } from 'class-validator'
 
-export interface IAuthLoginDto {
-  user: string
-  password: string
+export type AuthLoginProps = {
+  user: string | undefined
+  password: string | undefined
 }
 
-export class AuthLoginDto implements IAuthLoginDto {
+export class AuthLoginDto implements AuthLoginProps {
   @IsString()
+  @MaxLength(80)
   user: string
 
   @IsString()
+  @MaxLength(20)
   password: string
+
+  constructor(user: string, password: string) {
+    this.user = user.trim()
+    this.password = password
+  }
+}
+
+export class AuthLoginFactory {
+  static create(user: string, password: string): AuthLoginDto {
+    return new AuthLoginDto(user, password)
+  }
 }
