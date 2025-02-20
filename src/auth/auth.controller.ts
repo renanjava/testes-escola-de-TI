df172b8d1192b995067fa2a29c1a8d5bf5bc5adf
renanjava <renancvr777@gmail.com>
2025-02-20 14:30:38 -0300

    fix: globalpipes para dar erro 400 e não 500 nos bodies das requisições

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,8 @@
-import { IsEmail } from 'class-validator'
 import { Controller, Post, Body } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthLoginDto } from './dto/auth-login.dto'
 import { AuthRegisterDto } from './dto/auth-register.dto'
 import { UserService } from '@/user/user.service'
-import { Prisma } from '@prisma/client'
 
 @Controller('auth')
 export class AuthController {
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common'
-import { CreateUserDto } from './dto/create-user.dto'
-import { UpdateUserDto } from './dto/update-user.dto'
 import { PrismaService } from '../prisma.service'
 import { User, Prisma } from '@prisma/client'
 
