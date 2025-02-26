969cb3755b9f8c5788bf09f6b5a9598cde871ad5
renanjava <renancvr777@gmail.com>
2025-02-26 15:25:27 -0300

    chore: configurando ambientes

--- src/main.ts
+++ src/main.ts
@@ -37,8 +37,18 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService)
   const PORT = configService.get<number>('PORT') || 3000
+  const environment = configService.get<string>('NODE_ENV') || 'development'
+  let databaseUrl = configService.get<string>('DATABASE_URL')
 
   await app.listen(PORT)
+  if (environment === 'development') {
+    Logger.log('Aplicação rodando em desenvolvimento')
+    databaseUrl = databaseUrl?.replace('postgres:5432', 'localhost:5432')
+  } else if (environment === 'production') {
+    Logger.log('Aplicação rodando em produção')
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    databaseUrl = databaseUrl?.replace('localhost:5432', 'postgres:5432')
+  }
   Logger.log(`🚀 Server running on http://localhost:${PORT}`)
 }
 
