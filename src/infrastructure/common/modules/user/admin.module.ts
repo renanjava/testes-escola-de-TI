6b6010607412d24da537fac95abaa7a245be031f
renanjava <renancvr777@gmail.com>
2025-04-08 21:30:13 -0300

    feat: prisma module para seguir o design pattern singleton

--- src/infrastructure/common/modules/user/admin.module.ts
+++ src/infrastructure/common/modules/user/admin.module.ts
@@ -3,19 +3,19 @@ import { AdminController } from '@/infrastructure/controllers/admin/admin.contro
 import { ManagerService } from '@/infrastructure/services/user/manager.service'
 import { UserService } from '@/infrastructure/services/user/user.service'
 import { UserRepositoryImpl } from '@/infrastructure/repositories/user/impl-user.repository'
-import { PrismaService } from '@/infrastructure/services/orm/prisma.service'
 import { BakeryManagerService } from '@/infrastructure/services/bakery/bakery-manager.service'
 import { BakeryManagerRepositoryImpl } from '@/infrastructure/repositories/bakery/impl-bakery-manager.repository'
 import { BakeryRepositoryImpl } from '@/infrastructure/repositories/bakery/impl-bakery.repository'
 import { BakeryService } from '@/infrastructure/services/bakery/bakery.service'
+import { PrismaModule } from '../orm/prisma.module'
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AdminController],
   providers: [
     ManagerService,
     UserService,
     UserRepositoryImpl,
-    PrismaService,
     BakeryManagerService,
     BakeryManagerRepositoryImpl,
     BakeryRepositoryImpl,
--- src/infrastructure/common/modules/user/user.module.ts
+++ src/infrastructure/common/modules/user/user.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common'
 import { UserRepositoryImpl } from '@/infrastructure/repositories/user/impl-user.repository'
-import { PrismaService } from '@/infrastructure/services/orm/prisma.service'
 import { UserController } from '@/infrastructure/controllers/user/user.controller'
 import { UserService } from '@/infrastructure/services/user/user.service'
 import { JwtService } from '@nestjs/jwt'
+import { PrismaModule } from '../orm/prisma.module'
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UserController],
-  providers: [UserRepositoryImpl, PrismaService, UserService, JwtService],
+  providers: [UserRepositoryImpl, UserService, JwtService],
   exports: [UserRepositoryImpl],
 })
 export class UserModule {}
