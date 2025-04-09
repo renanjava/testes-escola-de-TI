dd18c127680f617ac783340d15ae7cb1f613e65e
renanjava <renancvr777@gmail.com>
2025-04-09 20:24:39 -0300

    feat: Factory para criar os UseCases de User para a controller

--- src/infrastructure/common/modules/user/user.module.ts
+++ src/infrastructure/common/modules/user/user.module.ts
@@ -4,11 +4,18 @@ import { UserController } from '@/infrastructure/controllers/user/user.controlle
 import { UserService } from '@/infrastructure/services/user/user.service'
 import { JwtService } from '@nestjs/jwt'
 import { PrismaModule } from '../orm/prisma.module'
+import { UserUseCasesFactory } from '@/infrastructure/factories/user/user-use-cases.factory'
 
 @Module({
   imports: [PrismaModule],
   controllers: [UserController],
-  providers: [UserRepositoryImpl, UserService, JwtService],
+  providers: [
+    UserRepositoryImpl,
+    UserService,
+    JwtService,
+    { provide: 'UserRepository', useClass: UserRepositoryImpl },
+    UserUseCasesFactory,
+  ],
   exports: [UserRepositoryImpl],
 })
 export class UserModule {}
