53b76181d92698f1bb83d8ec40208f298cde6594
renanjava <renancvr777@gmail.com>
2025-04-21 19:41:23 -0300

    feat: usecase gerartoken, abstração de métodos da infrastructure e transferido a lógica de login na service pra usecase de login

---
+++ src/application/usecases/auth/gerar-token.use-case.ts
@@ -0,0 +1,18 @@
+import type { IUserPayload } from '@/application/controllers/interfaces/user-payload.interface'
+import type IUseCases from '../interfaces/use-cases.interface'
+import type { TokenProps } from '@/application/controllers/interfaces/token-props.interface'
+import type { IAccessToken } from './interfaces/access-token.interface'
+
+export class GerarTokenUseCase implements IUseCases {
+  constructor(private readonly iAccessToken: IAccessToken) {}
+  execute(iUserPayload: IUserPayload): TokenProps {
+    const payload = {
+      username: iUserPayload.username,
+      sub: iUserPayload.sub,
+      role: iUserPayload.role,
+    }
+    return {
+      access_token: this.iAccessToken.sign(payload),
+    }
+  }
+}
--- src/infrastructure/services/auth/auth.service.ts
+++ src/infrastructure/services/auth/auth.service.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { UserRepositoryImpl } from '@/infrastructure/repositories/user/impl-user.repository'
-import { Password } from '@/shared/common/utils/password'
-import { IUserPayload } from '@/application/controllers/interfaces/user-payload.interface'
 import { NodemailerService } from '../email/nodemailer.service'
 import UserEntity from '@/domain/user/entities/user.entity'
 import CreateUserUseCase from '@/application/usecases/user/create-user.use-case'
-import { SenhaInvalidaException } from '@/infrastructure/exceptions/user/senha-invalida.exception'
 import { TokenProps } from '@/application/controllers/interfaces/token-props.interface'
 import UserLoginEntity from '@/domain/user/entities/user-login.entity'
 import { User } from '@prisma/client'
@@ -14,6 +11,7 @@ import { AuthRegisterAdapter } from '@/infrastructure/adapters/auth/auth-registe
 import { AuthRegisterProps } from '@/infrastructure/dtos/auth/auth-register.dto'
 import UserLoginUseCase from '@/application/usecases/auth/user-login.use-case'
 import { SendEmailUseCase } from '@/application/usecases/email/send-email.use-case'
+import { GerarTokenUseCase } from '@/application/usecases/auth/gerar-token.use-case'
 
 @Injectable()
 export class AuthService {
@@ -25,16 +23,10 @@ export class AuthService {
 
   async loginUser(inputUser: UserLoginEntity): Promise<TokenProps> {
     const userLoginUseCase = new UserLoginUseCase(this.userRepository)
+    const gerarTokenUseCase = new GerarTokenUseCase(this.jwtService)
     const userFounded = (await userLoginUseCase.execute(inputUser)) as User
-    const validPassword = await Password.verify(
-      inputUser.password,
-      userFounded.password,
-    )
-    if (!validPassword) {
-      throw new SenhaInvalidaException()
-    }
 
-    return this.generateToken({
+    return gerarTokenUseCase.execute({
       sub: userFounded.id,
       username: userFounded.username,
       role: userFounded.role,
@@ -47,16 +39,5 @@ export class AuthService {
     const sendEmailUseCase = new SendEmailUseCase(this.nodemailerService)
     sendEmailUseCase.execute(registeredUser.email)
     return AuthRegisterAdapter.toResponse(registeredUser)
-  }
-
-  generateToken(user: IUserPayload): TokenProps {
-    const payload = {
-      username: user.username,
-      sub: user.sub,
-      role: user.role,
-    }
-    return {
-      access_token: this.jwtService.sign(payload),
-    }
   }
 }
